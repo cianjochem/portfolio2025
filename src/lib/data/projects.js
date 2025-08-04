a9d03fc9fd3a20b44b13caf306d6a7c86458788c
cianjochem <jochemcian@gmail.com>
2025-08-04 19:40:15 +0200

    project content update

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -1,77 +1,53 @@
 // src/lib/data/projects.js
 export const projects = [
 	{
-		title: 'Suffocating Waters',
-		year: 2020,
-		type: ['Data Visualization', 'Print'],
-		slug: 'suffocating-waters',
-		description: `Fertilizer run-off from industrial agriculture is choking the planet’s oceans, rivers and lakes. Nitrogen and phosphorus pollution feed explosive algal blooms that suck the oxygen from the water as they grow. These algal blooms result in dead zones that pose an ever-increasing threat to marine life as their habitats disappear.
-
-The publication ›Suffocating Waters‹ aims to draw the reader's attention to this issue through tailored infographics and map materials. The specially developed imagery shows the processes on a narrative level and supports the text. The maps provide quantitative information and are intended to encourage readers to conduct their investigations into the correlations between nitrate levels and declining oxygen levels.`,
-		mentors: 'Prof. Marion Fink',
-		cooperation: 'Laura Quade',
-		vimeo: '512526817',
-		teaserImage: '/projects/suffocating-waters/teaser.png',
+		title: 'Nuclear bomb',
+		year: 2025,
+		type: ['Illustration'],
+		slug: 'nuclear-bomb',
+		description: '',
+		cooperation: 'Jonas Oesch, Joana Kelén, Leonid Leiva Ariosa',
+		vimeo: '',
+		article:
+			'https://www.nzz.ch/wissenschaft/iran-und-die-atombombe-wie-realistisch-ist-der-bau-heute-ld.1891834',
+		teaserImage: '/projects/nuclear-bomb/iranian-nuclear-program-1.png',
 		images: [
-			'/projects/suffocating-waters/dsc00501-min-1200x.png',
-			'/projects/suffocating-waters/dsc00492-min-1200x.png',
-			'/projects/suffocating-waters/dsc00523-min-1200x.png'
+			'/projects/nuclear-bomb/iranian-nuclear-program-1.png',
+			'/projects/nuclear-bomb/nuclear-bomb-1.png'
 		]
 	},
 	{
-		title: 'WindDisplay',
-		year: 2021,
-		type: ['Generative Tool'],
-		slug: 'wind-display',
-		description: `›WindDisplay‹ is an online tool that shows the current wind conditions worldwide. The typography is influenced by real-time data, such as wind strength and wind direction. It should show experimentally how data could be visualized in other ways and how typography can communicate information only through visual appearance. The tool can be individualized by different sliders to change the location or the style of visualization.`,
-		mentors: 'Ted Davies',
-		cooperation: 'Alick Rodinova',
-		vimeo: '538419044',
-		teaserImage: '/projects/wind-display/teaser.png',
-		images: ['/projects/wind-display/teaser.png']
-	},
-	{
-		title: 'Gässli Film Festival',
-		year: 2018,
-		type: ['Poster', 'Motion'],
-		slug: 'gaessli-film-festival',
-		description: `The ›Gässli Film Festival‹ is a short film festival held annually in the ›Gerbergässlein‹ in Basel. As a non-profit event in the Basel region, it conveys cinematic fascination and backgrounds on filmmaking. \n\n The Festival should be communicated through a keyvisual, which works as a poster and as moving media in the form of a teaser. The keyvisual is based on typographic experiments with a tube screen in 4:3 format. This resulted in moving typographic compositions, which should remind the viewer of the nostalgia in the filmmaking of the 80s.`,
-		mentors: 'Prof Marion Fink, Dirk Koy, Fabian Kempter',
-		cooperation: 'Salome Habersatter',
-		vimeo: '343040619',
-		teaserImage: '/projects/gässli-film-festival/dsc00604-min-1200x.png',
-		images: ['/projects/gässli-film-festival/dsc00604-min-1200x.png']
-	},
-	{
-		title: 'Poster Slam 16',
-		year: 2018,
-		type: ['Poster', 'Motion'],
-		slug: 'poster-slam-16',
-		description: `The ›Posterslam‹ is a platform to experiment with poster design on a given theme and discuss the result over a beer. The topic of the 16th ›Posterslam‹ dealt with the organic architecture of the US-American architect Frank Lloyd Wright. The poster references the ›Solomon R. Guggenheim Museum‹ in New York City. The 3D rendering and typographic composition create a contrast between the organic and the spatial.`,
-		vimeo: '',
-		teaserImage: '/projects/poster-slam-16/dsc00659-min-1200x.png',
-		images: ['/projects/poster-slam-16/dsc00659-min-1200x.png']
-	},
-	{
-		title: 'Switzerland 2075',
-		year: 2024,
-		type: ['Data Visualization'],
-		slug: 'schweiz-2075',
-		description:
-			'How will climate change alter Switzerland’s landscape? In this immersive article, the reader travels along the Aare River in the year 2075 to explore how rising temperatures affect water supply and demand. \n\n A 3D map guides the journey, with satellite imagery altered based on scientific data to show how the landscape might look 50 years from now. At two locations—one in the Alps and one in a forest—the reader dives down to ground level: they peer into a new lake where a glacier once stood and walk along the ridge of a transformed forest. Data visualizations and geographic overlays on the map provide scientific context to the journey. The article reveals how much might change—and how early adaptation and decisive action could still make our future livable.',
-		cooperation: 'Adina Renner, Sven Titz, Michel Graustück, Tom Vaillant, John Cors',
-		vimeo: '1106720209',
+		title: 'Rubaya Mine',
+		year: 2025,
+		type: ['Data Visualization', '3D Animation', 'Mapping'],
+		slug: 'rubaya-mine',
+		description: '',
+		cooperation: 'Adina Renner, Samuel Misteli, Arlette Bashizi',
+		vimeo: '1106734298',
 		article:
-			'https://www.nzz.ch/english/switzerland-in-2075-a-climate-change-visualization-ld.1855325',
-		teaserImage: '/projects/schweiz-2075/ch2075-teaser.png',
+			'https://www.nzz.ch/english/from-congos-rebel-held-mine-to-the-wests-phones-ld.1881059',
+		teaserImage: '/projects/rubaya-mine/rubaya-mine-teaser.png',
 		images: [
-			'/projects/schweiz-2075/3-cw-teaser.png',
-			'/projects/schweiz-2075/1-cw-teaser.png',
-			'/projects/schweiz-2075/making-of-6 Kopie@3x.png',
-			'/projects/schweiz-2075/2-cw-teaser.png',
-			'/projects/schweiz-2075/5-cw-teaser.png',
-			'/projects/schweiz-2075/making-of-6 Kopie 2@3x.png',
-			'/projects/schweiz-2075/4-cw-teaser.png'
+			'/projects/rubaya-mine/Bildschirmfoto 2025-08-04 um 14.35.15.png',
+			'/projects/rubaya-mine/Bildschirmfoto 2025-08-04 um 14.35.15.png',
+			'/projects/rubaya-mine/Bildschirmfoto 2025-08-04 um 14.35.15.png'
+		]
+	},
+	{
+		title: 'Greenland Deal',
+		year: 2025,
+		type: ['Data Visualization', 'Mapping'],
+		slug: 'greenland-deal',
+		description: '',
+		cooperation: 'Andreas Rüesch',
+		vimeo: '',
+		article:
+			'https://www.nzz.ch/international/trump-und-groenland-strategische-rolle-aber-auch-prestigeprojekt-ld.1876958',
+		teaserImage: '/projects/greenland-deal/greenland-deal-teaser.png',
+		images: [
+			'/projects/greenland-deal/greenland-deal-1.png',
+			'/projects/greenland-deal/greenland-deal-2.png',
+			'/projects/greenland-deal/greenland-deal-3.png'
 		]
 	},
 	{
@@ -89,47 +65,116 @@ The publication ›Suffocating Waters‹ aims to draw the reader's attention to 
 		images: ['/projects/forgotten-wars/en_fw@3x.png']
 	},
 	{
-		title: 'Greenland Deal',
-		year: 2025,
-		type: ['Data Visualization', 'Mapping'],
-		slug: 'greenland-deal',
-		description: '',
-		cooperation: 'Andreas Rüesch',
-		vimeo: '',
+		title: 'Switzerland 2075',
+		year: 2024,
+		type: ['Data Visualization'],
+		slug: 'schweiz-2075',
+		description:
+			'How will climate change alter Switzerland’s landscape? In this immersive article, the reader travels along the Aare River in the year 2075 to explore how rising temperatures affect water supply and demand. \n\n A 3D map guides the journey, with satellite imagery altered based on scientific data to show how the landscape might look 50 years from now. At two locations—one in the Alps and one in a forest—the reader dives down to ground level: they peer into a new lake where a glacier once stood and walk along the ridge of a transformed forest. Data visualizations and geographic overlays on the map provide scientific context to the journey. The article reveals how much might change—and how early adaptation and decisive action could still make our future livable.',
+		cooperation: 'Adina Renner, Sven Titz, Michel Graustück, Tom Vaillant, John Cors',
+		vimeo: '1106720209',
 		article:
-			'https://www.nzz.ch/international/trump-und-groenland-strategische-rolle-aber-auch-prestigeprojekt-ld.1876958',
-		teaserImage: '/projects/greenland-deal/greenland-teaser.png',
+			'https://www.nzz.ch/english/switzerland-in-2075-a-climate-change-visualization-ld.1855325',
+		teaserImage: '/projects/schweiz-2075/forgotten-wars-teaser.png',
 		images: [
-			'/projects/greenland-deal/ff42fc9dffbfb98a85c0a48280422d7d_kw.png',
-			'/projects/greenland-deal/ff42fc9dffbfb98a85c0a4828042d217_cw.png',
-			'/projects/greenland-deal/d6388fe6a215a72ecb46a581aced2975_cw.png'
+			'/projects/schweiz-2075/3-cw-teaser.png',
+			'/projects/schweiz-2075/1-cw-teaser.png',
+			'/projects/schweiz-2075/making-of-6 Kopie@3x.png',
+			'/projects/schweiz-2075/2-cw-teaser.png',
+			'/projects/schweiz-2075/5-cw-teaser.png',
+			'/projects/schweiz-2075/making-of-6 Kopie 2@3x.png',
+			'/projects/schweiz-2075/4-cw-teaser.png'
 		]
 	},
 	{
-		title: 'Iranian Nuclear Program',
-		year: 2025,
-		type: ['Illustration'],
-		slug: 'iranian-nuclear-program',
+		title: 'Bahnhofstrasse',
+		year: 2024,
+		type: ['Data Visualization', '3D Animation'],
+		slug: 'bahnhofstrasse',
 		description: '',
-		cooperation: 'Jonas Oesch, Joana Kelén, Leonid Leiva Ariosa',
-		vimeo: '',
+		cooperation: 'Simon Huwyler (Code)',
 		article:
-			'https://www.nzz.ch/wissenschaft/iran-und-die-atombombe-wie-realistisch-ist-der-bau-heute-ld.1891834',
-		teaserImage: '/projects/iranian-nuclear-program/iran-atomprogramm-teaser.png',
-		images: ['/projects/iranian-nuclear-program/iran-atomprogramm.png']
+			'https://www.nzz.ch/zuerich/gold-gucci-und-gabbana-dieses-3-d-modell-zeigt-wie-die-zuercher-bahnhofstrasse-ein-ort-fuer-reiche-wurde-ld.1839429',
+		teaserImage: '/projects/bahnhofstrasse/bahnhofstrasse-teaser.png',
+		images: [
+			'/projects/bahnhofstrasse/bahnhofstrasse-1.png',
+			'/projects/bahnhofstrasse/bahnhofstrasse-2.png',
+			'/projects/bahnhofstrasse/bahnhofstrasse-3.png'
+		]
 	},
 	{
-		title: 'Rubaya Mine',
-		year: 2025,
-		type: ['Data Visualization', '3D Animation', 'Mapping'],
-		slug: 'rubaya-mine',
-		description: '',
-		cooperation: 'Adina Renner, Samuel Misteli, Arlette Bashizi',
-		vimeo: '1106734298',
-		article:
-			'https://www.nzz.ch/english/from-congos-rebel-held-mine-to-the-wests-phones-ld.1881059',
-		teaserImage: '/projects/rubaya-mine/rubaya-teaser.png',
-		images: ['/projects/rubaya-mine/rubaya-1.png']
+		title: 'Densification',
+		year: 2022,
+		type: ['Data Visualization', 'Illustration'],
+		slug: 'densification',
+		description: ``,
+		cooperation: 'Giorgio Scherrer (Text)',
+		teaserImage: '/projects/densification/verdichtung-teaser.png',
+		images: ['/projects/densification/densification-titel.gif']
+	},
+	{
+		title: 'WindDisplay',
+		year: 2021,
+		type: ['Generative Tool'],
+		slug: 'wind-display',
+		description: `›WindDisplay‹ is an online tool that shows the current wind conditions worldwide. The typography is influenced by real-time data, such as wind strength and wind direction. It should show experimentally how data could be visualized in other ways and how typography can communicate information only through visual appearance. The tool can be individualized by different sliders to change the location or the style of visualization.`,
+		mentors: 'Ted Davies',
+		cooperation: 'Alick Rodinova',
+		vimeo: '538419044',
+		teaserImage: '/projects/wind-display/wind-display-teaser.png',
+		images: ['/projects/wind-display/teaser.png']
+	},
+	{
+		title: 'Suffocating Waters',
+		year: 2020,
+		type: ['Data Visualization', 'Print'],
+		slug: 'suffocating-waters',
+		description: `Fertilizer run-off from industrial agriculture is choking the planet’s oceans, rivers and lakes. Nitrogen and phosphorus pollution feed explosive algal blooms that suck the oxygen from the water as they grow. These algal blooms result in dead zones that pose an ever-increasing threat to marine life as their habitats disappear. The publication ›Suffocating Waters‹ aims to draw the reader's attention to this issue through tailored infographics and map materials. The specially developed imagery shows the processes on a narrative level and supports the text. The maps provide quantitative information and are intended to encourage readers to conduct their investigations into the correlations between nitrate levels and declining oxygen levels.`,
+		mentors: 'Prof. Marion Fink',
+		cooperation: 'Laura Quade',
+		vimeo: '512526817',
+		teaserImage: '/projects/suffocating-waters/teaser.png',
+		images: [
+			'/projects/suffocating-waters/dsc00501-min-1200x.png',
+			'/projects/suffocating-waters/dsc00492-min-1200x.png',
+			'/projects/suffocating-waters/dsc00523-min-1200x.png'
+		]
+	},
+	{
+		title: 'Poster Slam 16',
+		year: 2019,
+		type: ['Poster', 'Motion'],
+		slug: 'poster-slam-16',
+		description: `The ›Posterslam‹ is a platform to experiment with poster design on a given theme and discuss the result over a beer. The topic of the 16th ›Posterslam‹ dealt with the organic architecture of the US-American architect Frank Lloyd Wright. The poster references the ›Solomon R. Guggenheim Museum‹ in New York City. The 3D rendering and typographic composition create a contrast between the organic and the spatial.`,
+		vimeo: '',
+		teaserImage: '/projects/poster-slam-16/posterslam-16-teaser.png',
+		images: ['/projects/poster-slam-16/dsc00659-min-1200x.png']
+	},
+	{
+		title: 'Disappearing Places',
+		year: 2020,
+		type: ['Mapping', 'Print'],
+		slug: 'disappearing-places',
+		description: `Once the fourth largest lake in the world, the Aral Sea now covers only about 10% of its original area. The consequences for local communities, who traditionally lived from fishing, are severe. Former coastal zones have turned into pesticide-contaminated salt deserts, leading to widespread respiratory health issues. \n\n Three articles from different time periods form a chronological text layer, revealing that awareness of the issue existed as early as the 1980s — when the crisis was still widely downplayed or ignored. The amount of text per page gradually decreases throughout the publication, mirroring the shrinking of the lake. Readers must increasingly exert effort to access the content, just as fishermen once had to travel farther to reach the receding shoreline. The text is supplemented by cartographic elements and photography.`,
+		vimeo: '512519533',
+		teaserImage: '/projects/disappearing-places/dsc00558-min.png',
+		images: [
+			'/projects/disappearing-places/dsc00546-min.png',
+			'/projects/disappearing-places/dsc00558-min.png',
+			'/projects/disappearing-places/dsc00563-min.jpg'
+		]
+	},
+	{
+		title: 'Gässli Film Festival',
+		year: 2018,
+		type: ['Poster', 'Motion'],
+		slug: 'gaessli-film-festival',
+		description: `The ›Gässli Film Festival‹ is a short film festival held annually in the ›Gerbergässlein‹ in Basel. As a non-profit event in the Basel region, it conveys cinematic fascination and backgrounds on filmmaking. \n\n The Festival should be communicated through a keyvisual, which works as a poster and as moving media in the form of a teaser. The keyvisual is based on typographic experiments with a tube screen in 4:3 format. This resulted in moving typographic compositions, which should remind the viewer of the nostalgia in the filmmaking of the 80s.`,
+		mentors: 'Prof Marion Fink, Dirk Koy, Fabian Kempter',
+		cooperation: 'Salome Habersatter',
+		vimeo: '343040619',
+		teaserImage: '/projects/gässli-film-festival/gaessli-film-festival-teaser.png',
+		images: ['/projects/gässli-film-festival/dsc00604-min-1200x.png']
 	}
 	// Weitere Projekte...
 ];
